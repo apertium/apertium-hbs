3d31867c0aeea8f5fcf180f2e094ee62522e5573
Nikola Ljubešić <nljubesi@gmail.com>
2016-03-25 12:54:45 +0000

    Minor purist pronoun intervention

--- export-lexicon.ap-me.SR.sh
+++ export-lexicon.ap-me.SR.sh
@@ -3,6 +3,3 @@ grep -v ' lang="hr"' apertium-hbs.hbs.metadix > apertium-hbs.hbs_SR_purist.metad
 xsltproc --stringparam alt hbs_SR_purist --stringparam var ek alt.xsl apertium-hbs.hbs_SR_purist.metadix > .deps/apertium-hbs.hbs_SR_purist.dix
 rm apertium-hbs.hbs_SR_purist.metadix
 lt-expand .deps/apertium-hbs.hbs_SR_purist.dix | python export-lexicon.ap-me.py | gzip -c > apertium-hbs.hbs_SR_purist.mte.gz
-#awk '{if (substr($3,1,1)!="P") print $0}' apertium-hbs.hbs_SR_purist.mte > temp
-#cat MTE_pronouns.txt >> temp
-#mv temp apertium-hbs.hbs_SR_purist.mte
