5cdea19d229f3a4f6bc9e2a7596e93d71c008c29
Nikola Ljubešić <nljubesi@gmail.com>
2022-12-01 14:34:52 +0000

    Updates shell scripts for py3

--- export-lexicon.ap-me.HR.sh
+++ export-lexicon.ap-me.HR.sh
@@ -2,4 +2,4 @@
 grep -v ' lang="sr"' apertium-hbs.hbs.metadix > apertium-hbs.hbs_HR_purist.metadix
 xsltproc --stringparam alt hbs_HR_purist --stringparam var ijek alt.xsl apertium-hbs.hbs_HR_purist.metadix > .deps/apertium-hbs.hbs_HR_purist.dix
 rm apertium-hbs.hbs_HR_purist.metadix
-lt-expand .deps/apertium-hbs.hbs_HR_purist.dix | python export-lexicon.ap-me.py | gzip -c > apertium-hbs.hbs_HR_purist.mte.gz
+lt-expand .deps/apertium-hbs.hbs_HR_purist.dix | python export-lexicon.ap-me.py3.py | gzip -c > apertium-hbs.hbs_HR_purist.mte.gz
--- export-lexicon.ap-me.SR.sh
+++ export-lexicon.ap-me.SR.sh
@@ -2,4 +2,4 @@
 grep -v ' lang="hr"' apertium-hbs.hbs.metadix > apertium-hbs.hbs_SR_purist.metadix
 xsltproc --stringparam alt hbs_SR_purist --stringparam var ek alt.xsl apertium-hbs.hbs_SR_purist.metadix > .deps/apertium-hbs.hbs_SR_purist.dix
 rm apertium-hbs.hbs_SR_purist.metadix
-lt-expand .deps/apertium-hbs.hbs_SR_purist.dix | python export-lexicon.ap-me.py | gzip -c > apertium-hbs.hbs_SR_purist.mte.gz
+lt-expand .deps/apertium-hbs.hbs_SR_purist.dix | python export-lexicon.ap-me.py3.py | gzip -c > apertium-hbs.hbs_SR_purist.mte.gz
